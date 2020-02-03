cb5673843f0a26ceb16abd28cb1a73b56b57be06
Mehedi Hasan <m.mehedi_hasan@hotmail.com>
2020-02-03 16:53:55 +0600

    Initial Commit

---
+++ DelegateProtocol/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DelegateProtocol
+//
+//  Created by Md. Mehedi Hasan on 3/2/20.
+//  Copyright © 2020 Md. Mehedi Hasan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
